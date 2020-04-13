9ed7be2a0cf0a2d6d0c61868496e353207ebc679
Alex Scott <alexanderscott@outlook.com>
2020-04-13 12:25:29 +0200

    WIP on tracking all attributes

--- src/job.ts
+++ src/job.ts
@@ -2,11 +2,19 @@ import * as vscode from "vscode";
 
 export class Job {
 	constructor(
-		private readonly originalText: string,
 		public readonly job_name: string,
 		public readonly parent_name: string,
 		public readonly job_name_location: vscode.Range,
 		public readonly parent_name_location: vscode.Range,
 		public readonly document: vscode.TextDocument
+	) {}
+}
+
+export class JobAttribute {
+	constructor(
+		public readonly attribute_key: string,
+		public readonly attribute_value: string,
+		public readonly attribute_location: vscode.Range,
+		public readonly document: vscode.Uri
 	) {}
 }
--- src/job_parser.ts
+++ src/job_parser.ts
@@ -1,10 +1,11 @@
 import * as vscode from "vscode";
-import { Job } from "./job";
+import { Job, JobAttribute } from "./job";
 
 export class JobParser {
 	private job_regex = /^- job:/gm;
 	private job_name_regex = /(?<=name:).*/gm;
 	private job_parent_regex = /(?<=parent:).*/gm;
+	private special_attribute_keys = ["name", "parent"];
 
 	parse_job_from_line_number(textDocument: vscode.TextDocument, job_line_number: number): Job | undefined {
 		let job_name = null;
@@ -28,6 +29,7 @@ export class JobParser {
 				parent_name_location = line;
 				continue;
 			}
+			this.parse_job_attribute_from_line(line, textDocument);
 			line_number_iterator++;
 			if (this.at_the_end_of_job_definition(textDocument, line_number_iterator)) {
 				break;
@@ -42,14 +44,7 @@ export class JobParser {
 			job_name_location !== null &&
 			parent_name_location !== null
 		) {
-			return new Job(
-				job_name_location.text,
-				job_name,
-				parent_name,
-				job_name_location.range,
-				parent_name_location.range,
-				textDocument
-			);
+			return new Job(job_name, parent_name, job_name_location.range, parent_name_location.range, textDocument);
 		}
 
 		line_number_iterator = job_line_number;
@@ -67,6 +62,7 @@ export class JobParser {
 				parent_name_location = line;
 				continue;
 			}
+			this.parse_job_attribute_from_line(line, textDocument);
 			line_number_iterator--;
 			if (this.at_the_end_of_job_definition(textDocument, line_number_iterator)) {
 				break;
@@ -81,17 +77,32 @@ export class JobParser {
 			job_name_location !== null &&
 			parent_name_location !== null
 		) {
-			return new Job(
-				job_name_location.text,
-				job_name,
-				parent_name,
-				job_name_location.range,
-				parent_name_location.range,
-				textDocument
-			);
+			return new Job(job_name, parent_name, job_name_location.range, parent_name_location.range, textDocument);
 		}
 
 		return undefined;
+	}
+
+	parse_job_attribute_from_line(
+		job_line: vscode.TextLine,
+		textDocument: vscode.TextDocument
+	): JobAttribute | undefined {
+		let attribute_key = job_line.text.substr(0, job_line.text.indexOf(":"));
+		let attribute_value = job_line.text.substr(job_line.text.indexOf(":") + 1);
+		if (attribute_key && attribute_value) {
+			attribute_key = attribute_key.replace(/\s/g, "");
+			attribute_value = this.remove_spaces_from_special_value(attribute_key, attribute_value);
+			console.log(attribute_key + ":" + attribute_value);
+			return new JobAttribute(attribute_key, attribute_value, job_line.range, textDocument.uri);
+		}
+		return undefined;
+	}
+
+	remove_spaces_from_special_value(attribute_key: string, attribute_value: string): string {
+		if (this.special_attribute_keys.includes(attribute_key)) {
+			return attribute_value.replace(/\s/g, "");
+		}
+		return attribute_value;
 	}
 
 	parse_parent_name_from_line_number(textDocument: vscode.TextDocument, job_line_number: number): string | undefined {
