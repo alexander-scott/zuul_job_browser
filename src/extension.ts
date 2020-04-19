6e0780921176b3fe88a8318c7b3ef3a7b42e9f46
Alex Scott <alexanderscott@outlook.com>
2020-04-19 09:52:37 +0200

    Created a file manager class

--- src/extension.ts
+++ src/extension.ts
@@ -1,122 +1,64 @@
 import * as vscode from "vscode";
 import { JobHierarchyProvider } from "./providers/job_hierarchy_provider";
-import { JobDefinitionparser } from "./job_parsing/job_definition_parser";
 import { JobDefinitionProvider } from "./providers/job_definition_provider";
 import { JobHoverProvider } from "./providers/job_hover_provider";
 import { JobReferencesProvider } from "./providers/job_references_provider";
-import { JobDefinitionManager } from "./job_parsing/job_definition_manager";
 import { JobSymbolWorkspaceDefinitionsProvider } from "./providers/job_symbol_workspace_definitions_provider";
 import { JobSymbolDocumentDefinitionsProvider } from "./providers/job_symbol_document_definitions_provider";
-import { ProjectTemplateParser } from "./project_template_parsing/project_template_parser";
-import { ProjectTemplateJobManager } from "./project_template_parsing/project_template_job_manager";
-import { DocType } from "./doc_type";
+import { FileManager } from "./file_manager";
 
-const job_manager = new JobDefinitionManager();
-const project_template_job_manager = new ProjectTemplateJobManager();
 const workspace_pattern = "**/zuul.d/*.yaml";
+const file_manager = new FileManager(workspace_pattern);
 
 export function activate(context: vscode.ExtensionContext) {
-	build_job_hierarchy();
+	file_manager.build_job_hierarchy();
 
 	context.subscriptions.push(
 		vscode.languages.registerCallHierarchyProvider(
 			{ scheme: "file", language: "yaml" },
-			new JobHierarchyProvider(job_manager)
+			new JobHierarchyProvider(file_manager.get_job_manager())
 		)
 	);
 	context.subscriptions.push(
 		vscode.languages.registerDefinitionProvider(
 			{ scheme: "file", language: "yaml" },
-			new JobDefinitionProvider(job_manager, project_template_job_manager)
+			new JobDefinitionProvider(file_manager.get_job_manager(), file_manager.get_project_template_mannager())
 		)
 	);
 	context.subscriptions.push(
 		vscode.languages.registerHoverProvider(
 			{ scheme: "file", language: "yaml" },
-			new JobHoverProvider(job_manager, project_template_job_manager)
+			new JobHoverProvider(file_manager.get_job_manager(), file_manager.get_project_template_mannager())
 		)
 	);
 	context.subscriptions.push(
 		vscode.languages.registerReferenceProvider(
 			{ scheme: "file", language: "yaml" },
-			new JobReferencesProvider(job_manager, project_template_job_manager)
+			new JobReferencesProvider(file_manager.get_job_manager(), file_manager.get_project_template_mannager())
 		)
 	);
 	context.subscriptions.push(
 		vscode.languages.registerDocumentSymbolProvider(
 			{ scheme: "file", language: "yaml" },
-			new JobSymbolDocumentDefinitionsProvider(job_manager)
+			new JobSymbolDocumentDefinitionsProvider(file_manager.get_job_manager())
 		)
 	);
 	context.subscriptions.push(
-		vscode.languages.registerWorkspaceSymbolProvider(new JobSymbolWorkspaceDefinitionsProvider(job_manager))
+		vscode.languages.registerWorkspaceSymbolProvider(
+			new JobSymbolWorkspaceDefinitionsProvider(file_manager.get_job_manager())
+		)
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand("zuulplugin.rebuild-hierarchy", () => {
-			build_job_hierarchy();
+			file_manager.build_job_hierarchy();
 		})
 	);
 
-	const workspace = vscode.workspace.workspaceFolders![0];
-	let filewatcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(workspace, workspace_pattern));
-	filewatcher.onDidChange((doc) => update_job_hierarchy_after_file_changed(doc));
-	filewatcher.onDidCreate((doc) => update_job_hierarchy_after_file_created(doc));
-	filewatcher.onDidDelete((doc) => update_job_hierarchy_after_files_deleted(doc));
-}
-
-function build_job_hierarchy() {
-	job_manager.remove_all_jobs();
-	if (vscode.workspace.workspaceFolders) {
-		vscode.workspace.workspaceFolders.forEach((workspace) => {
-			vscode.workspace.findFiles(new vscode.RelativePattern(workspace, workspace_pattern)).then((results) => {
-				results.forEach(async (doc_uri) => {
-					let document = await vscode.workspace.openTextDocument(doc_uri);
-					if (DocType.is_a_project_template(document)) {
-						ProjectTemplateParser.parse_project_template_in_document(document, project_template_job_manager);
-					} else {
-						JobDefinitionparser.parse_job_definitions_in_document(document, job_manager);
-					}
-				});
-			});
-		});
-	}
-	console.log("Finished building job hierarchy");
-}
-
-async function update_job_hierarchy_after_file_changed(doc: vscode.Uri) {
-	let document = await vscode.workspace.openTextDocument(doc);
-	if (project_template_job_manager.is_known_file(doc)) {
-		console.log("Starting updating project template in " + doc.path);
-		project_template_job_manager.remove_all_jobs_in_document(doc);
-		ProjectTemplateParser.parse_project_template_in_document(document, project_template_job_manager);
-		console.log("Finished updating project template in " + doc.path);
-	} else if (job_manager.is_known_file(doc)) {
-		console.log("Starting updating jobs in " + doc.path);
-		job_manager.remove_all_jobs_in_document(doc);
-		JobDefinitionparser.parse_job_definitions_in_document(document, job_manager);
-		console.log("Finished updating jobs in " + doc.path);
-	} else {
-		update_job_hierarchy_after_file_created(doc);
-	}
-}
-async function update_job_hierarchy_after_file_created(doc: vscode.Uri) {
-	let document = await vscode.workspace.openTextDocument(doc);
-	if (DocType.is_a_project_template(document)) {
-		console.log("Starting parsing project template in " + doc.path);
-		ProjectTemplateParser.parse_project_template_in_document(document, project_template_job_manager);
-		console.log("Finished parsing project template in " + doc.path);
-	} else {
-		console.log("Starting parsing jobs in " + doc.path);
-		JobDefinitionparser.parse_job_definitions_in_document(document, job_manager);
-		console.log("Finished parsing jobs in " + doc.path);
-	}
-}
-function update_job_hierarchy_after_files_deleted(doc_uri: vscode.Uri) {
-	console.log("Removing jobs in: " + doc_uri.path);
-	job_manager.remove_all_jobs_in_document(doc_uri);
-	project_template_job_manager.remove_all_jobs_in_document(doc_uri);
+	file_manager.set_file_watchers();
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	file_manager.destroy();
+}
---
+++ src/file_manager.ts
@@ -0,0 +1,95 @@
+import * as vscode from "vscode";
+import { JobDefinitionManager } from "./job_parsing/job_definition_manager";
+import { ProjectTemplateJobManager } from "./project_template_parsing/project_template_job_manager";
+import { ProjectTemplateParser } from "./project_template_parsing/project_template_parser";
+import { DocType } from "./doc_type";
+import { JobDefinitionparser } from "./job_parsing/job_definition_parser";
+
+export class FileManager {
+	private file_watchers: vscode.FileSystemWatcher[] = [];
+	private job_manager = new JobDefinitionManager();
+	private project_template_job_manager = new ProjectTemplateJobManager();
+
+	constructor(private readonly workspace_pattern: string) {}
+
+	destroy() {
+		this.file_watchers.forEach((file_watcher) => {
+			file_watcher.dispose();
+		});
+	}
+
+	build_job_hierarchy() {
+		this.job_manager.remove_all_jobs();
+		if (vscode.workspace.workspaceFolders) {
+			vscode.workspace.workspaceFolders.forEach((workspace) => {
+				vscode.workspace.findFiles(new vscode.RelativePattern(workspace, this.workspace_pattern)).then((results) => {
+					results.forEach(async (doc_uri) => {
+						let document = await vscode.workspace.openTextDocument(doc_uri);
+						if (DocType.is_a_project_template(document)) {
+							ProjectTemplateParser.parse_project_template_in_document(document, this.project_template_job_manager);
+						} else {
+							JobDefinitionparser.parse_job_definitions_in_document(document, this.job_manager);
+						}
+					});
+				});
+			});
+		}
+		console.log("Finished building job hierarchy");
+	}
+
+	set_file_watchers() {
+		if (vscode.workspace.workspaceFolders) {
+			vscode.workspace.workspaceFolders.forEach((workspace) => {
+				let file_watcher = vscode.workspace.createFileSystemWatcher(
+					new vscode.RelativePattern(workspace, this.workspace_pattern)
+				);
+				file_watcher.onDidChange((doc) => this.update_job_hierarchy_after_file_changed(doc));
+				file_watcher.onDidCreate((doc) => this.update_job_hierarchy_after_file_created(doc));
+				file_watcher.onDidDelete((doc) => this.update_job_hierarchy_after_files_deleted(doc));
+				this.file_watchers.push(file_watcher);
+			});
+		}
+	}
+
+	async update_job_hierarchy_after_file_changed(doc: vscode.Uri) {
+		let document = await vscode.workspace.openTextDocument(doc);
+		if (this.project_template_job_manager.is_known_file(doc)) {
+			console.log("Starting updating project template in " + doc.path);
+			this.project_template_job_manager.remove_all_jobs_in_document(doc);
+			ProjectTemplateParser.parse_project_template_in_document(document, this.project_template_job_manager);
+			console.log("Finished updating project template in " + doc.path);
+		} else if (this.job_manager.is_known_file(doc)) {
+			console.log("Starting updating jobs in " + doc.path);
+			this.job_manager.remove_all_jobs_in_document(doc);
+			JobDefinitionparser.parse_job_definitions_in_document(document, this.job_manager);
+			console.log("Finished updating jobs in " + doc.path);
+		} else {
+			this.update_job_hierarchy_after_file_created(doc);
+		}
+	}
+	async update_job_hierarchy_after_file_created(doc: vscode.Uri) {
+		let document = await vscode.workspace.openTextDocument(doc);
+		if (DocType.is_a_project_template(document)) {
+			console.log("Starting parsing project template in " + doc.path);
+			ProjectTemplateParser.parse_project_template_in_document(document, this.project_template_job_manager);
+			console.log("Finished parsing project template in " + doc.path);
+		} else {
+			console.log("Starting parsing jobs in " + doc.path);
+			JobDefinitionparser.parse_job_definitions_in_document(document, this.job_manager);
+			console.log("Finished parsing jobs in " + doc.path);
+		}
+	}
+	update_job_hierarchy_after_files_deleted(doc_uri: vscode.Uri) {
+		console.log("Removing jobs in: " + doc_uri.path);
+		this.job_manager.remove_all_jobs_in_document(doc_uri);
+		this.project_template_job_manager.remove_all_jobs_in_document(doc_uri);
+	}
+
+	get_job_manager(): JobDefinitionManager {
+		return this.job_manager;
+	}
+
+	get_project_template_mannager(): ProjectTemplateJobManager {
+		return this.project_template_job_manager;
+	}
+}
